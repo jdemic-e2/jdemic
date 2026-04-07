a55c7d603c571b647ec93d37e1e6f654e62f81b6
Nicolaiciuc Andrei <scutynicolaiciuc@gmail.com>
2026-04-07 12:27:31 +0300

    updated game manager and gamestate

--- src/jdemic/GameLogic/GameManager.java
+++ src/jdemic/GameLogic/GameManager.java
@@ -6,17 +6,6 @@ import jdemic.GameLogic.ServerRelatedClasses.GameState;
 
 public class GameManager {
     GameState state;
-    DiseaseManager diseaseManager;
-    Deck cardDeck;
-    List<Player> players;
-    PandemicMapGraph map;
-
-    int currentPlayerIndex;
-    int actionsRemaining;
-    int infectionRate;
-    int epidemicCount;
-    boolean gameOver;
-    boolean gameWon;
 
     private static final int ACTIONS_PER_TURN = 4;
     private static final int[] INFECTION_RATE_TRACK = {2, 2, 2, 3, 3, 4, 4};
@@ -25,16 +14,16 @@ public class GameManager {
     public GameManager(List<Player> players) 
     {
         this.state = new GameState(); 
-        this.map = new PandemicMapGraph();
-        this.diseaseManager = new DiseaseManager();
-        this.players = players;
-        this.cardDeck = new Deck(this);
-        this.currentPlayerIndex = 0;
-        this.actionsRemaining = ACTIONS_PER_TURN;
-        this.infectionRate = 0;
-        this.epidemicCount = 0;
-        this.gameOver = false;
-        this.gameWon = false;
+        state.setMap(new PandemicMapGraph());
+        state.setDiseaseManager(new DiseaseManager());
+        state.setPlayers(players);
+        state.setCardDeck(new Deck(this));
+        state.setCurrentPlayerIndex(0);
+        state.setActionsRemaining(ACTIONS_PER_TURN);
+        state.setInfectionRate(0);
+        state.setEpidemicCount(0);
+        state.setGameOver(false);
+        state.setGameWon(false);
 
         setupGame();
     }
@@ -42,100 +31,100 @@ public class GameManager {
     private void setupGame()
     {
 
-        CityNode atlanta = map.getCity("Atlanta");
+        CityNode atlanta = state.getMap().getCity("Atlanta");
         atlanta.addResearchStation();
 
-        for(Player player : players)
+        for(Player player : state.getPlayers())
         {
             state.addPlayer(player.getState());
-            player.deckReference = cardDeck;
+            player.deckReference = state.getCardDeck();
         }
     }
 
     public void performAction(Player player, GameAction action)
     {
-        if(gameOver) return;
-        if(actionsRemaining <= 0) return;
+        if(state.isGameOver()) return;
+        if(state.getActionsRemaining() <= 0) return;
 
         if(action.isValid(state))
         {
             action.execute(state, player.getState());
-            actionsRemaining--;
+            state.setActionsRemaining(state.getActionsRemaining() - 1);
         }
     }
 
     public void nextTurn()
     {
-        if(gameOver) return;
+        if(state.isGameOver()) return;
 
-        Player current = players.get(currentPlayerIndex);
+        Player current = state.getPlayers().get(state.getCurrentPlayerIndex());
 
-        current.drawCards(cardDeck);
+        current.drawCards(state.getCardDeck());
 
-        if(cardDeck.getRemainingCardsCount() <= 0)
+        if(state.getCardDeck().getRemainingCardsCount() <= 0)
         {
-            gameOver = true;
-            gameWon = false;
+            state.setGameOver(true);
+            state.setGameWon(false);
             return;
         }
 
         checkWinCondition();
         checkLoseCondition();
 
-        if(gameOver) return;
+        if(state.isGameOver()) return;
 
-        currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
-        actionsRemaining = ACTIONS_PER_TURN;
+        state.setCurrentPlayerIndex((state.getCurrentPlayerIndex() + 1) % state.getPlayers().size());
+        state.setActionsRemaining(ACTIONS_PER_TURN);
     }
 
     public void checkWinCondition()
     {
-        if(diseaseManager.isCured())
+        if(state.getDiseaseManager().isCured())
         {
-            gameOver = true;
-            gameWon = true;
+            state.setGameOver(true);
+            state.setGameWon(true);
         }
     }
 
     public void checkLoseCondition(){
 
-        if(diseaseManager.getOutbreakScore() >= MAX_OUTBREAKS)
+        if(state.getDiseaseManager().getOutbreakScore() >= MAX_OUTBREAKS)
         {
-            gameOver = true;
-            gameWon = false;
+            state.setGameOver(true);
+            state.setGameWon(false);
         }
 
-        if(cardDeck.getRemainingCardsCount() <= 0)
+        if(state.getCardDeck().getRemainingCardsCount() <= 0)
         {
-            gameOver = true;
-            gameWon = false;
+            state.setGameOver(true);
+            state.setGameWon(false);
         }
     }
 
     public int getInfectionRate()
     {
-        if(infectionRate >= INFECTION_RATE_TRACK.length) return INFECTION_RATE_TRACK[INFECTION_RATE_TRACK.length - 1];
-        return INFECTION_RATE_TRACK[infectionRate];
+        if(state.getInfectionRate() >= INFECTION_RATE_TRACK.length) return INFECTION_RATE_TRACK[INFECTION_RATE_TRACK.length - 1];
+        return INFECTION_RATE_TRACK[state.getInfectionRate()];
     }
 
     public void increaseInfectionRate()
     {
-        infectionRate++;
+        state.setInfectionRate(state.getInfectionRate() + 1);
     }
 
     public Player getCurrentPlayer()
     {
-        return players.get(currentPlayerIndex);
+        return state.getPlayers().get(state.getCurrentPlayerIndex());
     }
 
     public boolean isGameOver()
     {
-        return gameOver;
+        return state.isGameOver();
     }
 
     public boolean isGameWon()
     {
-        return gameWon;
+        return state.isGameWon();
     }
 
     public void syncState()
